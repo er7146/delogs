38a261a71e500ca59cbfce0b50ccc7592ce4be65
Eddie <eddie@Eddies-MacBook-Pro.local>
2014-12-03 20:05:54 -0700

    confi.v2.1

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module App
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+     config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+}
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -19,10 +22,6 @@ module App
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
-    config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
 
   end
 end
