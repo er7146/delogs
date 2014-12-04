b500214033d0da4f329c5486b0edd1d4bde0e774
Eddie <eddie@Eddies-MacBook-Pro.local>
2014-12-03 20:14:38 -0700

    config.v2.4

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,7 @@ module App
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
-}
+    config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
